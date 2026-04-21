4636a46bd5187602ba71b4574b540c30e6dcd580
mwsundberg <gitcommits@mws.is>
2026-04-20 17:47:50 -0700

    add maplibregl map with mapterhorn hillshades

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import './App.css'
+import './App.css';
 import { useState } from 'react';
-import { Splitter } from './components/Splitter'
+import { Splitter } from './components/Splitter';
+
+import { Layer, Map, Source} from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 function App() {
 
   const [split, setSplit] = useState(0.5);
+  const [mapViewState, setMapViewState] = useState({
+    longitude: -113.6302593801,
+    latitude: 35.81025936,
+    zoom: 11,
+  });
+
   
   return (
     <Splitter vertical split={0.8} slot1={
       <Splitter split={split} slot1={
         <>
           Map
-          <input type='range' min={0} max={100} value={split * 100} onChange={(ev) => setSplit(parseInt(ev.target.value) / 100)} />
+          <Map
+            reuseMaps
+            {...mapViewState}
+            onMove={(ev)=>setMapViewState(ev.viewState)}>
+              <Source id='mapterhorn-dem' type='raster-dem' tiles={['https://tiles.mapterhorn.com/{z}/{x}/{y}.webp']} encoding='terrarium' tileSize={512} attribution='<a href="https://mapterhorn.com/attribution">© Mapterhorn</a>'>
+                <Layer id='hillshade' type='hillshade' paint={{'hillshade-method': 'igor'}} />
+              </Source>
+          </Map>
         </>
       } slot2={
         <>
