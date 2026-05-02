e4fdc7dbf9a6f99446d8a712bcb81f371855126b
mwsundberg <gitcommits@mws.is>
2026-05-02 06:09:24 -0700

    added line visualization with a canvas

--- src/audioGeneration.ts
+++ src/audioGeneration.ts
@@ -2,4 +2,6 @@ import type { Line } from "./App";
 
 export function playLine(line: Line) {
     console.log('played line ' + line.id, line.elevations);
+
+    /* TODO */
 }
--- src/components/LineRenderer.tsx
+++ src/components/LineRenderer.tsx
@@ -1,11 +1,58 @@
-import type { Line } from "../App"
+import { useEffect, useRef, useState } from 'react'
+import type { Line } from '../App'
 
 interface LineRendererProps {
     line: Line|undefined
 }
 
-export function LineRenderer({line}: LineRendererProps) {
+
+export function LineRenderer({line, ...props}: LineRendererProps) {    
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    /* Listen to get the current dimensions with a ResizeObserver */
+    const [[width, height], setDimensions] = useState<[number, number]>([0, 0]);
+    useEffect(()=>{
+        /* Set up the ResizeObserver */
+        if (canvasRef.current) {
+            const resizeObserver = new ResizeObserver((entries)=>{
+                for(const entry of entries) {
+                    if(entry.contentRect) setDimensions([entry.contentRect.width, entry.contentRect.height])
+                }
+            });
+            resizeObserver.observe(canvasRef.current);
+
+            return ()=>resizeObserver.disconnect();
+        }
+    }, []);
+
+    /* Rerender whenever the dimensions or line changes */
+    useEffect(()=>{
+        if(canvasRef.current && width && height) {
+            const ctx = canvasRef.current.getContext('2d')!;
+            ctx.clearRect(0, 0, width, height);
+
+            /* Draw the elevations profile */
+            if(line) {
+                const [minElevation, maxElevation] = line.elevations.reduce((acc, current) =>
+                    [(current < acc[0])? current : acc[0],
+                     (current > acc[1])? current : acc[1]],
+                    [Infinity, -Infinity]);
+
+                ctx.beginPath();
+                ctx.fillStyle = 'white';
+                ctx.moveTo(0, height);
+                for(let i = 0; i < line.elevations.length; i++) {
+                    ctx.lineTo(width * (i / (line.elevations.length - 1)), height - height * ((line.elevations[i] - minElevation) / (maxElevation - minElevation)))
+                }
+                ctx.lineTo(width, height);
+                ctx.closePath();
+                ctx.fill();
+
+            }
+        }
+    }, [width, height, line]);
+
     return (
-        <div>Line Render: {line?.elevations.join(', ')}</div>
+        <canvas ref={canvasRef} width={width} height={height} {...props} style={{width: '100%', height: '100px'}}></canvas>
     )
 }
