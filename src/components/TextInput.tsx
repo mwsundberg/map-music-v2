d8076b8632facf0a99be59b8599d039783f7188e
mwsundberg <gitcommits@mws.is>
2026-06-18 18:16:48 -0700

    added escape and enter key functionality to text inputs that deselects the element and (in escape's case) restores the initial value

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+
+type TextInputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 const InputStyled = styled.input`
 	background-color: transparent;
 	border: none;
 	color: var(--foreground-text);
 	border-bottom: 2px solid var(--foreground);
-	-webkit-appearance: textfield;
+
+	&:not(:focus-within) {
+		-webkit-appearance: textfield;
+	}
 
 	&:focus-within {
 		outline: none;
@@ -13,6 +19,44 @@ const InputStyled = styled.input`
 	}
 `;
 
-export default function TextInput(props: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) {
-	return <InputStyled type='text' {...props} />;
+/* Get the native input value setter instead of react's overwritten one */
+/* Trick taken from SO: https://stackoverflow.com/a/46012210/3196151 */
+/* @ts-ignore */
+const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+
+export default function TextInput({value, onFocus, onKeyDown, ...props}: TextInputProps) {
+	/* Store the state on focus so it can be restored if the user hits escape */
+	const [initialState, setInitialState] = useState(value);
+
+	return <InputStyled type='text' value={value}
+		onFocus={(ev) => {
+			/* Store the initial state */
+			setInitialState(ev.target.value);
+
+			/* Call the onFocus of the caller */
+			if(onFocus) onFocus(ev);
+		}}
+		onKeyDown={(ev)=>{
+			/* Listen for enter or escape */
+			if(ev.key === 'Enter') {
+				/* Deselect the input */
+				/* @ts-ignore (Typescript isn't aware this is an input element) */
+				ev.target.blur();
+
+			} else if(ev.key === 'Escape') {
+				/* Restore initial state */
+				/* Trick taken from SO: https://stackoverflow.com/a/46012210/3196151 */
+				/* @ts-ignore */
+				nativeInputValueSetter.call(ev.target, initialState);
+				ev.target.dispatchEvent(new Event('input', { bubbles: true }));
+
+				/* Deselect the input */
+				/* @ts-ignore (Typescript isn't aware this is an input element) */
+				ev.target.blur();
+			}
+
+			/* Call the onKeyDown of the caller */
+			if(onKeyDown) onKeyDown(ev);
+		}}
+	{...props} />;
 }
